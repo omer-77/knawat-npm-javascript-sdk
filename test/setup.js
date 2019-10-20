cc64765d8580de5a141cb30e7d84fce2966989de
Knawat <Abdelrahman@knawat.com>
2019-10-20 18:57:47 +0300

    test: update test cases

--- test/setup.js
+++ test/setup.js
@@ -2,10 +2,8 @@ import MP from '../src/index.js';
 
 MP.baseUrl = 'https://dev.mp.knawat.io/api';
 const instance = {
-  key: 'ffdc11c6-b31c-4f81-8f67-468cf776e096',
-  secret: 'b86820ca-ac8c-4af8-ba86-340d57036de7',
-  token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZmZGMxMWM2LWIzMWMtNGY4MS04ZjY3LTQ2OGNmNzc2ZTA5NiIsImV4cCI6MTU2MDA4NTQ0NCwiaWF0IjoxNTU0OTAxNDQ0fQ.OeDcUrinNmhxxzutza0u0Uz3Tw_AOJxJJTveGXQJ07I'
+  key: '6df6d032-169b-4174-a02e-d943a2557419',
+  secret: 'dd8deab0-6dbe-11e9-9f00-035f917f36f6'
 };
 const mp = new MP(instance);
 export default mp;
