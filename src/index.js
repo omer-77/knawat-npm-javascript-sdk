ac16d13d20c64529f50b98393957ce4b4a47b957
Knawat <Abdelrahman@knawat.com>
2019-10-20 20:23:53 +0300

    feat: add membeship api methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import KnawatMP from './KnawatMP';
 import categories from './api/categories';
 import invoices from './api/invoices';
+import membership from './api/membership';
 import orders from './api/orders';
 import products from './api/products';
 
 KnawatMP._addToPrototype(categories);
 KnawatMP._addToPrototype(invoices);
+KnawatMP._addToPrototype(membership);
+KnawatMP._addToPrototype(miscellaneous);
 KnawatMP._addToPrototype(orders);
 KnawatMP._addToPrototype(products);
 
