aa306c5ba5ee03c779c41c283c502acd1a8e4107
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2019-10-21 15:11:22 +0300

    Update setup.js

--- test/setup.js
+++ test/setup.js
@@ -3,7 +3,7 @@ import KnawatMP from '../src/index.js';
 KnawatMP.baseUrl = 'https://dev.mp.knawat.io/api';
 const instance = {
   key: '6df6d032-169b-4174-a02e-d943a2557419',
-  secret: 'dd8deab0-6dbe-11e9-9f00-035f917f36f6'
+  secret: 'dd8deab0-6dbe-11e9-9f00-035f917f36f6',
 };
 const mp = new KnawatMP(instance);
 export default mp;
