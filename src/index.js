8106bfa0fa4df6bd2d2ffe7d619328e02e15e80d
Knawat <Abdelrahman@knawat.com>
2019-11-29 14:57:39 +0300

    fix: add missing apis

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import KnawatMP from './KnawatMP';
 import categories from './api/categories';
+import coupons from './api/coupons';
 import invoices from './api/invoices';
 import membership from './api/memberships';
 import miscellaneous from './api/miscellaneous';
@@ -7,8 +8,10 @@ import orders from './api/orders';
 import products from './api/products';
 import shipping from './api/shipping';
 import stores from './api/stores';
+import subscriptions from './api/subscriptions';
 
 KnawatMP._addToPrototype(categories);
+KnawatMP._addToPrototype(coupons);
 KnawatMP._addToPrototype(invoices);
 KnawatMP._addToPrototype(membership);
 KnawatMP._addToPrototype(miscellaneous);
@@ -16,5 +19,6 @@ KnawatMP._addToPrototype(orders);
 KnawatMP._addToPrototype(products);
 KnawatMP._addToPrototype(shipping);
 KnawatMP._addToPrototype(stores);
+KnawatMP._addToPrototype(subscriptions);
 
 module.exports = KnawatMP;
