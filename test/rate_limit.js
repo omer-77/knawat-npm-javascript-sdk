4833b7006652c7375788d444b434dc8b6705ea2a
Hazem Khaled <hazem.khaled@gmail.com>
2020-12-11 07:45:42 +0300

    Fix API rate limit

--- test/rate_limit.js
+++ test/rate_limit.js
@@ -4,37 +4,18 @@ import mp from './setup';
  * Test Knawat API limit
  */
 
-const executionTimeAr = [];
-jest.setTimeout(10000);
-test('Stopcock Test API 1st Call', () => {
-  return mp.getProducts(10, 1, null, null, 0).then((res) => {
-    executionTimeAr.push(new Date());
-    expect(res).toBeDefined();
-  });
-});
-test('Stopcock Test API 2nd Call ', () => {
-  return mp.getProducts(10, 1, null, null, 0).then((res) => {
-    executionTimeAr.push(new Date());
-    expect(res).toBeDefined();
-  });
-});
-test('Stopcock Test API 3rd Call', () => {
-  return mp.getProducts(10, 1, null, null, 0).then((res) => {
-    executionTimeAr.push(new Date());
-    expect(res).toBeDefined();
-  });
-});
-test('Stopcock Test API 4th Call', () => {
-  return mp.getProducts(10, 1, null, null, 0).then((res) => {
-    executionTimeAr.push(new Date());
-    let ApiSlot1 = diff_seconds(executionTimeAr[1], executionTimeAr[0]);
-    let ApiSlot2 = diff_seconds(executionTimeAr[3], executionTimeAr[2]);
-    expect(res).toBeDefined();
-    expect(ApiSlot2).not.toEqual(ApiSlot1);
-  });
-});
+const callsToTest = 30;
+jest.setTimeout(callsToTest * 1000);
+test(`Throttling ${callsToTest} requests in ${
+  callsToTest / 2 - 1
+}+ seconds`, async (done) => {
+  // call endpoint x times, expect to be finish in x / 2 since we have 2 requests per second
+  const startDate = new Date();
+  return Promise.allSettled(
+    [...Array(callsToTest).keys()].map(() => mp.getProducts())
+  ).then(() => {
+    expect((new Date() - startDate) / 1000).toBeGreaterThanOrEqual(callsToTest / 2 - 3);
 
-function diff_seconds(dt2, dt1) {
-  var diff = (new Date(dt2).getTime() - new Date(dt1).getTime()) / 1000;
-  return Math.abs(Math.round(diff));
-}
+    done();
+  });
+});
