d37cbbb6806024b01a1f8e23b1e45f60f127947d
Hazem Khaled <hazem.khaled@gmail.com>
2020-12-11 13:13:01 +0300

    refreshToken before test rate limit

--- test/rate_limit.js
+++ test/rate_limit.js
@@ -11,11 +11,12 @@ test(`Throttling ${callsToTest} requests in ${
 }+ seconds`, async (done) => {
   // call endpoint x times, expect to be finish in x / 2 since we have 2 requests per second
   const startDate = new Date();
-  return Promise.all([...Array(callsToTest).keys()].map(() => mp.getProducts())).then(
-    () => {
-      expect((new Date() - startDate) / 1000).toBeGreaterThanOrEqual(callsToTest / 2 - 3);
+  return mp
+    .refreshToken()
+    .then(() => Promise.all([...Array(callsToTest).keys()].map(() => mp.getProducts())))
+    .then(() => {
+      expect((new Date() - startDate) / 1000).toBeGreaterThanOrEqual(callsToTest / 2 - 1);
 
       done();
-    }
-  );
+    });
 });
